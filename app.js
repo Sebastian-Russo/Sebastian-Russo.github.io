86e262a7a55531fbcbadd40591304d62d8fbd498
Sebastian Russo <russo.sebastian@gmail.com>
2020-09-28 13:59:38 -0400

    updated menu button to say close also

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const handleBurger = () => {
     console.log('clicked')
     $('.nav-list').toggleClass('hidden');
+
+    if ($('.nav-list').hasClass('hidden')) {
+        $('#hamburger').html("");
+        $('#hamburger').html('Menu');
+    } else {
+        $('#hamburger').html("");
+        $('#hamburger').html('Close');
+    }
+
 }
 
 $('#hamburger').click(() => handleBurger())
